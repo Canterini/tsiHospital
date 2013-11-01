4cf03e6416549dadba572d37dea45b7e7c588dab
elellop <pato.com.net@gmail.com>
2013-11-01 11:07:22 -0400

    Pequeña modificación (Corrección al error)

--- WindowsApplication2/FormAdministrarDepartamentos.vb
+++ WindowsApplication2/FormAdministrarDepartamentos.vb
@@ -23,16 +23,19 @@ Public Class FormAdministrarDepartamentos
 
     Private Sub b_agregar_Click(sender As Object, e As EventArgs) Handles btnAgregardepto.Click
         Try
+         
+
+
             Departamento.insertar(txtNombreDepto.Text) 'esto hace que onserte en la bd llamando tu funcion de insertar bll y bll lama a dal para cargar los datos de la base de datos
             MessageBox.Show("Departamento ingresado exitosamente", "Ingreso", MessageBoxButtons.OK, MessageBoxIcon.Information)
             conn.Close()
 
         Catch ex As SqlClient.SqlException ' en caso de erros existe una excepcion paraque no sse  caiga el programa
-            MessageBox.Show("El nombre ingresado ya existe", "Error Nombre", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            MessageBox.Show("El departamento ingresado ya existe", "Error Nombre", MessageBoxButtons.OK, MessageBoxIcon.Error)
             Me.txtNombreDepto.Text = ""
             conn.Close()
         Catch ex As ArgumentException
-            MessageBox.Show("Ingrese un nombre", "Error ", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            MessageBox.Show("Favor ingrese un departamento", "Error ", MessageBoxButtons.OK, MessageBoxIcon.Error)
             Me.txtNombreDepto.Text = ""
             conn.Close()
         End Try
